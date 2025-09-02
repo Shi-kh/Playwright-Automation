0cc377c317e50cb8d02af892f28bfe63ad78c9a9
Shi-kh <shikharathod462@gmail.com>
2025-09-02 16:45:25 +0530

    update the test case steps

--- tests/test_campaign_creation.py
+++ tests/test_campaign_creation.py
@@ -7,8 +7,8 @@ from utils import config
 
 def test_create_campaign(page):
     # Step 1 - Create Account & Login
-    signup = SignupPage(page)
-    signup.create_account(config.TEST_USER_EMAIL, config.TEST_USER_PASSWORD)
+    # signup = SignupPage(page)
+    # signup.create_account(config.TEST_FULLNAME, config.TEST_USER_EMAIL, config.TEST_PHONE_NUMBER,config.TEST_USER_PASSWORD)
 
     login = LoginPage(page)
     login.login(config.TEST_USER_EMAIL, config.TEST_USER_PASSWORD)
@@ -20,9 +20,10 @@ def test_create_campaign(page):
     # Step 3 - Campaign Flow
     campaign = CampaignPage(page)
     campaign.select_template()
-    campaign.select_sms_channel()
-    campaign.enter_campaign_title(config.CAMPAIGN_TITLE)
-    campaign.enter_contact_phone(config.TEST_PHONE_NUMBER)
-    campaign.edit_sms_content(config.SMS_MESSAGE)
-    campaign.test_sms(config.TEST_PHONE_NUMBER)
-    campaign.save_and_exit()
+    campaign.close_popup()
+    # campaign.select_sms_channel()
+    # campaign.enter_campaign_title(config.CAMPAIGN_TITLE)
+    # campaign.enter_contact_phone(config.TEST_PHONE_NUMBER)
+    # campaign.edit_sms_content(config.SMS_MESSAGE)
+    # campaign.test_sms(config.TEST_PHONE_NUMBER)
+    # campaign.save_and_exit()
